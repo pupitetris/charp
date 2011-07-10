917e43ed3cb054a8b6bcd7a09b318c79fb7822b7
Arturo Espinosa <pupitetris@yahoo.com>
2011-07-09 21:42:57 -0500

    Take connection, login and resources as parameters, just as the other client programs.

--- scripts/test-wget.sh
+++ scripts/test-wget.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+POSTDATA='login='$2'&res='$3'&params='$4
+
 tmp=/tmp/tmp-$RANDOM.test
-wget --post-data='login=testuser&res=user_auth&params=' http://www.myproject.local/request -O $tmp
+wget --post-data=$POSTDATA $1/request -O $tmp
 cat $tmp
 rm -f $tmp
 
