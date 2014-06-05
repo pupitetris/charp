329cc51ca2b63e0140d97a87707134817026b224
Arturo Espinosa <arturoea@gmail.com>
2014-06-05 13:54:45 -0500

    Call psql with min-messages on warning so that it doesn't show NOTICEs during automatic initialization.

--- pg/conf/scripts/config-script.sh
+++ pg/conf/scripts/config-script.sh
@@ -9,15 +9,9 @@ export PGUSER=${!CONF_USER}
 # This obscure function runs psql with our own set of configuration variables
 # and filters out unwanted psql NOTICEs.
 function db_client {
-    {
-		local sql_file=$1
-		shift
-		psql -q -f ${sql_file} "$@" 2>&1 >&3 3>&- | grep -v ''\
-'NOTICE:  \(function\|funci.n\) [^)]\+) \(does not exist, skipping\|no existe\)\|'\
-'NOTICE:  CREATE TABLE / PRIMARY KEY \(will create implicit index\|crear. el .ndice impl.cito\)\|'\
-'NOTICE:  \(constraint\|no existe la restricci.n\)\|'\
-'NOTICE:  \(view\|la vista\)' >&2 3>&-
-    } 3>&1
+	local sql_file=$1
+	shift
+	PGOPTIONS='--client-min-messages=warning' psql -q -f ${sql_file} "$@"
 }
 
 function db_initialize {
@@ -27,7 +21,7 @@ function db_initialize {
 	
 	# Check if we can initialize the database before proceeding with the
 	# rest of the SQL scripts so they don't fail.
-	psql -q -d postgres -U $DB_SUPERUSER -c "DROP DATABASE IF EXISTS $PGDATABASE"
+	PGOPTIONS='--client-min-messages=warning' psql -q -d postgres -U $DB_SUPERUSER -c "DROP DATABASE IF EXISTS $PGDATABASE"
 	
 	if psql -q -U $DB_SUPERUSER -c "SELECT procpid, application_name, client_addr FROM pg_stat_activity WHERE current_query NOT LIKE '% pg_stat_activity %';" 2>/dev/null; then
 		echo 'The database couldn''t be deleted, a client is still connected.' >&2
