2e8d1d208b2704ea070a5d9996e60dd5e62f2f3e
Arturo Espinosa <arturoea@gmail.com>
2014-06-07 18:24:08 -0500

    Bug fix when checking cnf permissions, and some pedantic code factorization.

--- mysql/conf/scripts/config-script.sh
+++ mysql/conf/scripts/config-script.sh
@@ -1,26 +1,28 @@
 # Sourced by conf/config.sh
 
-m4 -P "$DB_CONFIGDIR"/scripts/my-gen.cnf.m4 \
-	-D CONF_DATABASE="$CONF_DATABASE" \
-	-D CONF_HOST="$CONF_HOST" \
-	-D CONF_PORT="$CONF_PORT" \
-	-D CONF_USER="$CONF_USER" > "$DB_CONFIGDIR"/scripts/my-gen.cnf
+function process_cnf {
+	local file=$1
+	local user=$2
 
-m4 -P "$DB_CONFIGDIR"/scripts/my-gen.cnf.m4 \
-	-D CONF_DATABASE="$CONF_DATABASE" \
-	-D CONF_HOST="$CONF_HOST" \
-	-D CONF_PORT="$CONF_PORT" \
-	-D CONF_USER="$DB_SUPERUSER" > "$DB_CONFIGDIR"/scripts/my-su-gen.cnf
+	m4 -P -D CONF_DATABASE="$CONF_DATABASE" \
+		-D CONF_HOST="$CONF_HOST" \
+		-D CONF_PORT="$CONF_PORT" \
+		-D CONF_USER="$user" \
+		"$DB_CONFIGDIR"/scripts/my-gen.cnf.m4 > "$file"
+}
 
 function check_cnf_mod {
 	local file=$1
 
-	if $(stat -c %a "$file") != 600; then
+	if [ $(stat -c %a "$file") != 600 ]; then
 		echo File $file must have permissions 600. >&2
 		echo chmod 600 \'$file\' >&2
 		exit 4
 	fi
 }
+
+process_cnf "$DB_CONFIGDIR"/scripts/my-gen.cnf "$CONF_USER"
+process_cnf "$DB_CONFIGDIR"/scripts/my-su-gen.cnf "$DB_SUPERUSER"
 
 check_cnf_mod "$DB_CONFIGDIR"/my.cnf
 check_cnf_mod "$DB_CONFIGDIR"/my-su.cnf
