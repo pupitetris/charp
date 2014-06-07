4333eff1a615ce66e00024588b5f38b706fcfe6b
Arturo Espinosa <arturoea@gmail.com>
2014-06-07 15:08:25 -0500

    Some aesthetic changes to code and making sure that argument vars are not set from the outside.

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -15,7 +15,7 @@
 
 # Set the value of this variable to the name of the variable that points
 # to the project's code base.
-BASEDIR_VAR=CHARPDIR
+BASEDIR_VAR=CHARP_DIR
 
 # Set the locale you want the script to run under (comment for system default).
 export LANG="en_US.utf8"
@@ -23,29 +23,30 @@ export LC_ALL="en_US.utf8"
 
 # *** No further editing needed after this line. ***
 
-BASEDIR=${!BASEDIR_VAR}
-
+arg_DB=
 if [ "$1" = "-db" ]; then
     arg_DB=$2
     shift 2
 fi
 
+TESTDATA=
 if [ "$1" = "-td" ]; then
     TESTDATA=1
     shift
 fi
 
 # if -nocat, don't upload catalogs.
+NOCAT=
 if [ "$1" = "-nocat" ]; then
     NOCAT=1
     shift
 fi
 
+BASEDIR=${!BASEDIR_VAR}
 if [ -z "$BASEDIR" ]; then
     echo "$BASEDIR_VAR is not defined." >&2
     exit 1
 fi
-
 if [ ! -d "$BASEDIR" ]; then
     echo "The value of \$$BASEDIR_VAR ($BASEDIR) does not point to a directory." >&2
     exit 3
@@ -83,5 +84,5 @@ db_filter 05-functions.sql -su
 [ -e 06-catalogs.sql ] && [ -z "$NOCAT" ] && db_filter 06-catalogs.sql -su
 [ -e 07-views.sql ] && db_filter 07-views.sql
 [ -e 09-data.sql ] && db_filter 09-data.sql -su
-if [ -e 98-testdata.sql ]; then [ -z "$TESTDATA" ] || db_filter 98-testdata.sql -su; fi
-if [ -e 99-test.sql ]; then [ -z "$TESTDATA" ] || db_filter 99-test.sql -su; fi
+[ -e 98-testdata.sql ] && [ ! -z "$TESTDATA" ] && db_filter 98-testdata.sql -su
+[ -e 99-test.sql ] && [ ! -z "$TESTDATA" ] && db_filter 99-test.sql -su
