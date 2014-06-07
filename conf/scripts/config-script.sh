65938084ab433b66c8cd16980a0f0501f723f3a7
Arturo Espinosa <arturoea@gmail.com>
2014-06-07 11:30:43 -0500

    Check validity of CONF_DIR before sourcing config-script.sh, otherwise the check procedure's purpose is defeated. When generating the intermediate sql file, use a better tmp filename. General fix of use of quotes around variables.

--- conf/scripts/config-script.sh
+++ conf/scripts/config-script.sh
@@ -12,28 +12,18 @@ CONF_PORT=${!${PREFIX}_DB_PORT}
 CONF_USER=${!${PREFIX}_DB_USER}
 [ -z "$CONF_USER" ] && CONF_USER=${!DB_USER}
 
-# Define <PREFIX>DIR in your bash_profile.
-CONF_DIR=${!${PREFIX}DIR}
-
-if [ -z "$CONF_DIR" ]; then
-    echo Variable ${PREFIX}DIR is not defined. >&2
-    exit 1
-fi
-if [ -d "$CONF_DIR" ]; then
-    echo ${PREFIX}DIR value $CONF_DIR is not a directory. >&2
-    exit 1
-fi
+DB_CONFIGDIR=$BASEDIR/$DB_TYPE/conf
 
 BASEDIR=$CONF_DIR
-CONFIGDIR="$BASEDIR"/conf
-TESTDIR="$BASEDIR"/scripts/test
+CONFIGDIR=$BASEDIR/conf
+TESTDIR=$BASEDIR/scripts/test
 
 if [ $(uname -o) = 'Cygwin' ]; then
     IS_CYGWIN=1
 fi
 
 # Directory where the project's SQL is found.
-SQLDIR="$BASEDIR/$DB_TYPE/sql"
+SQLDIR=$BASEDIR/$DB_TYPE/sql
 
 if [ $DB_OS = "win" ]; then
 	# Transform the sql directory to Windows notation, 
@@ -41,20 +31,20 @@ if [ $DB_OS = "win" ]; then
 	WIN_SQLDIR=$(sed 's#/cygdrive/\(\w\+\)/#\1:/#' <<< "$SQLDIR")
 fi
 
-source $BASEDIR/$DB_TYPE/conf/scripts/config-script.sh
+source "$DB_CONFIGDIR"/scripts/config-script.sh
 
 sqlvars_end=$CONFIGDIR/scripts/sqlvars_end.m4
 if [ ! -e "$sqlvars_end" ]; then
 	echo 'm4_changecom(«--», «
 »)
 m4_divert«»m4_dnl
-m4_undefine(' > $sqlvars_end
+m4_undefine(' > "$sqlvars_end"
 	echo -n m4_dumpdef | m4 -P 2>&1 | grep -v '^\(m4_defn\|m4_dnl\):' | sed 's/^\([^:]\+\).*/		«\1»,/g' >> $sqlvars_end
 	echo '		«CONF_USER»,
 		«CONF_DATABASE»,
 		«CONF_LOCALE»,
 		«CONF_SQLDIR»,
-		«DEFINE»)«»m4_dnl' >> $sqlvars_end
+		«DEFINE»)«»m4_dnl' >> "$sqlvars_end"
 fi
 
 # This obscure function runs the db client with our own set of configuration variables
@@ -67,12 +57,14 @@ function db_filter {
 	local sqldir=$SQLDIR
 	if [ $DB_OS = "win" ]; then sqldir=$WIN_SQLDIR; fi
 
+	local tmp=${sql_file}-$RANDOM-tmp
+
 	m4 -P "$CONFIGDIR"/scripts/sqlvars_init.m4 \
-	    -D CONF_USER=$CONF_USER \
-	    -D CONF_DATABASE=$CONF_DATABASE \
+	    -D CONF_USER="$CONF_USER" \
+	    -D CONF_DATABASE="$CONF_DATABASE" \
 	    -D CONF_LOCALE="$DB_LOCALE" \
 	    -D CONF_SQLDIR="$sqldir" \
-	    "$CONFIGDIR"/sqlvars.m4 "$CONFIGDIR"/scripts/sqlvars_end.m4 "$sql_file" > ${sql_file}-tmp
-	db_client ${sql_file}-tmp "$@"
-	rm -f ${sql_file}-tmp
+	    "$CONFIGDIR"/sqlvars.m4 "$CONFIGDIR"/scripts/sqlvars_end.m4 "$sql_file" > "$tmp"
+	db_client "$tmp" "$@"
+	rm -f "$tmp"
 }
