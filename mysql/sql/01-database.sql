a23e3f1dafa6ad7ac23b8f54d111a68db87031e7
Arturo Espinosa <arturoea@gmail.com>
2014-06-10 23:12:32 -0500

    Added SQL routines for mysql to create the database unprivileged user.

--- mysql/sql/01-database.sql
+++ mysql/sql/01-database.sql
@@ -6,12 +6,30 @@
 --
 -- Licensed under the EUPL V.1.1. See the file LICENSE.txt for copying conditions.
 
-CREATE DATABASE M4_DEFN(dbname);
+M4_PROCEDURE(charp_create_user, «_username text, _passwd text», DETERMINISTIC MODIFIES SQL DATA, CURRENT_USER,
+			'Create user if it doesn''t exist',
+«_f: BEGIN 
+	   DECLARE _found integer;
+	   SELECT count(*) INTO _found FROM mysql.user WHERE user = _username;
+	   IF _found > 0 THEN LEAVE _f; END IF;
+
+	   set @s = concat('CREATE USER ''', _username, '''@''localhost'' IDENTIFIED BY ''', _passwd, '''');
+	   PREPARE _charp_create_user_prep FROM @s;
+	   EXECUTE _charp_create_user_prep;
+	   DEALLOCATE PREPARE _charp_create_user_prep;
+END»);
+
+CALL charp_create_user('M4_DEFN(user)', 'M4_DEFN(passwd)');
+DROP PROCEDURE charp_create_user;
+
+CREATE DATABASE M4_DEFN(dbname)
+	   CHARACTER SET = 'utf8'
+	   COLLATE = 'M4_DEFN(collate)';
 
 -- Connect to the newly created database for further configuration.
 \u M4_DEFN(dbname)
 
-M4_PROCEDURE(echo, msg text, DETERMINISTIC, M4_DEFN(myuser), 
+M4_PROCEDURE(echo, _msg text, DETERMINISTIC, M4_DEFN(myuser), 
 			'Simple function that allows us to send messages to the user''s console',
-			SELECT msg AS '');
+			SELECT _msg AS '');
 	
