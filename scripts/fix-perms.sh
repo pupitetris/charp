390903cb28514bd50aca4e99e1cc2de5891aa675
Arturo Espinosa <arturoea@gmail.com>
2014-06-18 17:39:53 -0500

    Set permissions of all files according to those indicated by git ls-tree.

--- scripts/fix-perms.sh
+++ scripts/fix-perms.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 chgrp -R Usuarios .
+
+git ls-tree -r HEAD | while read i; do 
+    mode=$(cut -c4-6 <<< $i)
+    file=$(cut -f4 -d$' ' <<< $i)
+    chmod $mode "$file"
+done
+
 chmod 600 mysql/conf/my.cnf
 chmod 600 mysql/conf/my-su.cnf
