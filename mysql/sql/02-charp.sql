68278f5f8b2b05fe32fa743e8a34e45721e92d3c
Arturo Espinosa <arturoea@gmail.com>
2014-06-19 20:36:14 -0500

    Finished basic CHARP stored procedures and functions for mysql, with a couple of details missing, but mostly done.

--- mysql/sql/02-charp.sql
+++ mysql/sql/02-charp.sql
@@ -10,7 +10,7 @@
 -- CHARP functions.
 
 
-M4_PROCEDURE( charp_log_error, «_code varchar(255), _username varchar(20), _ip_addr int(11), _res varchar(255), _msg text, _params text», 
+M4_PROCEDURE( charp_log_error, «_code varchar(255), _username varchar(20), _ip_addr integer unsigned, _res varchar(255), _msg text, _params text», 
 	      MODIFIES SQL DATA, M4_DEFN(myuser), 'Send an error report to the error log.', «
 BEGIN 
     INSERT INTO error_log VALUES(DEFAULT, CURRENT_TIMESTAMP, _code, _username, _ip_addr, _res, _msg, _params);
@@ -106,3 +106,121 @@ BEGIN
 	END IF;
 	RETURN _id;
 END;»);
+
+
+M4_FUNCTION( charp_rp_get_function_by_name, _function_name varchar(60),
+	     varchar(64), READS SQL DATA, M4_DEFN(myuser), «'Find given function with prefix rp_, raise PROCUNK if not found.'», «
+BEGIN
+	DECLARE	_name varchar(64);
+
+	SELECT routine_name INTO _name FROM information_schema.routines 
+	       WHERE routine_name = concat('rp_', _function_name) COLLATE utf8_general_ci;
+	IF _name IS NULL THEN
+	   CALL charp_raise1('PROCUNK', _function_name);
+	END IF;
+	RETURN _name;
+END;»);
+
+
+M4_FUNCTION( gen_random_bytes_hex, _count integer,
+	     text, NOT DETERMINISTIC NO SQL, M4_DEFN(myuser), 'Return _count random bytes in hex representation.', «
+BEGIN 
+	DECLARE _str text;
+
+	SET _str := '';
+	SET _count := _count * 2;
+	WHILE _count > 0 DO
+	      SET _str := concat(_str, hex(rand() * 15));
+	      SET _count := _count - 1;
+	END WHILE;
+
+	RETURN _str;
+END;»);
+
+
+M4_FUNCTION( charp_request_create, «_username varchar(20), _ip_addr integer unsigned, _function_name varchar(60), _params text»,
+	     text, NOT DETERMINISTIC MODIFIES SQL DATA, M4_DEFN(myuser), 'Registers a request returning a corresponding challlenge for the client to respond.', «
+BEGIN
+	DECLARE _random_bytes text;
+
+	SET _random_bytes := gen_random_bytes_hex(32);
+	INSERT INTO request VALUES(
+		_random_bytes, 
+		charp_account_get_id_by_username_status(_username, 'ACTIVE'),
+		CURRENT_TIMESTAMP,
+		_ip_addr,
+		charp_rp_get_function_by_name(_function_name),
+		_params
+	);
+	RETURN _random_bytes;
+END;»);
+
+
+M4_FUNCTION( charp_function_params, _function_name varchar(60),
+	     text, READS SQL DATA, M4_DEFN(myuser), 'Return the input parameter types that a given stored procedure requires.', «
+BEGIN
+	DECLARE	_fparams text;
+	DECLARE _fname text;
+
+	SELECT charp_rp_get_function_by_name (_function_name) INTO _fname;
+
+	SELECT group_concat(q.type SEPARATOR ',') INTO _fparams 
+	       FROM (SELECT IF (parameter_name = '_uid', 'UID',
+	       	    	        CASE data_type 
+				     WHEN 'int'     THEN 'INT' 
+			    	     WHEN 'varchar' THEN 'STR' 
+				     WHEN 'text'    THEN 'STR'
+				     WHEN 'boolean' THEN 'BOOL'
+				     WHEN 'date'    THEN 'DATE'
+				     ELSE 'STR'
+			        END) AS type
+			   FROM information_schema.parameters 
+			   WHERE specific_name = _fname AND ordinal_position > 0 
+			   ORDER BY ordinal_position
+		    ) AS q;
+
+	RETURN _fparams;
+END;»);
+
+
+M4_PROCEDURE( charp_request_check, «_username varchar(20), _ip_addr integer unsigned, _chal text, _hash text»,
+	      NOT DETERMINISTIC READS SQL DATA, M4_DEFN(myuser), 
+	      'Check that a given request is registered with the given data and compare the hash with one locally computed. Return the necessary data to execute.', «
+BEGIN
+	DECLARE _our_hash text;
+	DECLARE _user_id integer;
+	DECLARE _fname varchar(60);
+	DECLARE _fparams text;
+	DECLARE _req_params text;
+	DECLARE _passwd varchar(32);
+
+	SELECT 
+	       a.persona_id, 
+	       substring(p.proname FROM 4), 
+	       charp_get_function_params (p.proargtypes), 
+	       r.params, 
+	       a.passwd
+	       INTO _user_id, _fname, _fparams, _req_params, _passwd
+	       FROM request AS r NATURAL JOIN account AS a JOIN information_schema.routines AS p ON p.routine_name = r.proname
+	       WHERE a.username = _username AND
+		     r.request_id = _chal AND 
+		     r.ip_addr = _ip_addr;
+
+	IF _fname IS NULL THEN
+	   CALL charp_raise3('REQUNK', _username, _ip_addr, _chal);
+	END IF;
+
+	DELETE FROM request WHERE request_id = _chal;
+
+	SET _our_hash := sha2(concat(_username, _chal, _passwd), 256);
+	IF _our_hash <> _hash THEN
+	   CALL charp_raise3('REPFAIL', _username, _ip_addr, _chal);
+	END IF;
+
+	SELECT _user_id AS user_id, _fname AS fname, _fparams AS fparams, _req_params AS req_params;
+END;»);
+
+
+M4_FUNCTION( rp_user_auth, «», boolean, DETERMINISTIC NO SQL, M4_DEFN(myuser),
+	     «'Trivially return TRUE. If the user was authenticated, everything went OK with challenge-request sequence and there is nothing left to do: success.'»,
+	     «RETURN TRUE»);
