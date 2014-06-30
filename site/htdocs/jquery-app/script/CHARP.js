e84323086e8bbd8571aa8e1d6e389b291c45b1c1
Arturo Espinosa <arturoea@gmail.com>
2014-06-30 15:58:37 -0500

    Extend using the empty object as a base, so we get a merged copy of the second argument, instead of modifying it.

--- site/htdocs/jquery-app/script/CHARP.js
+++ site/htdocs/jquery-app/script/CHARP.js
@@ -71,15 +71,15 @@ CHARP.prototype = {
 	case 'success':
 	    return;
 	case 'error':
-	    err = $.extend ({ msg: 'Error HTTP: ' + req.statusText + ' (' + req.status + ').' }, this.ERRORS['HTTP:SRVERR']);
+	    err = $.extend ({}, { msg: 'Error HTTP: ' + req.statusText + ' (' + req.status + ').' }, this.ERRORS['HTTP:SRVERR']);
 	    break;
 	case 'parsererror':
 	    err = this.ERRORS['AJAX:JSON'];
 	    if (APP.DEVEL)
-		err = $.extend ({ msg: 'Datos: `' + req.responseText + '`.' }, err);
+		err = $.extend ({}, { msg: 'Datos: `' + req.responseText + '`.' }, err);
 	    break;
 	default:
-	    err = $.extend ({ msg: 'Error desconocido: (' + status + ').' }, this.ERRORS['AJAX:UNK']);
+	    err = $.extend ({}, { msg: 'Error desconocido: (' + status + ').' }, this.ERRORS['AJAX:UNK']);
 	}
 	this.handleError (err, ctx);
     },
