dde5c15cd8fb446b448f23b052770e88988e75a3
Arturo Espinosa <arturoea@gmail.com>
2014-12-02 18:55:29 -0600

    Implemented CHARP settings persistence through .Net's System.Configuration for Windows platform.

--- clients/monoCHARP/monoCHARP-gtk/MSConfig.cs
+++ clients/monoCHARP/monoCHARP-gtk/MSConfig.cs
@@ -1,60 +1,73 @@
 ﻿#if CHARP_WINDOWS
 using System;
+using System.Configuration;
 
 namespace monoCharp
 {
 	public partial class CharpGtk 
 	{
 		public class MSConfig : Charp.Config
-		{	
-			//		private GConf.Client gconf;
+		{
+			private static string CHARP_SECTION_NAME = "charp";
+			private Configuration config;
 			private string baseUrl;
 			private string baseHash;
 
 			public MSConfig (string baseUrl)
 			{
-			//			gconf = null;
 				this.baseUrl = baseUrl;
 				if (baseUrl != null)
 					baseHash = Charp.GetMD5HexHash (baseUrl);
 			}
 
-			private void Init ()
+			private KeyValueConfigurationCollection Init ()
 			{
-		/*			if (gconf == null) {
-						gconf = new GConf.Client ();
-					}*/
+				if (config == null) {
+					config = ConfigurationManager.OpenExeConfiguration (ConfigurationUserLevel.PerUserRoamingAndLocal);
+					if (config.Sections [CHARP_SECTION_NAME] == null) {
+						AppSettingsSection section = new AppSettingsSection ();
+						section.SectionInformation.AllowExeDefinition = ConfigurationAllowExeDefinition.MachineToLocalUser;
+						config.Sections.Add (CHARP_SECTION_NAME, section);
+						SuggestSync ();
+#if DEBUG
+						Console.WriteLine (config.FilePath);
+#endif
+					}
+				}
+				return (config.GetSection (CHARP_SECTION_NAME) as AppSettingsSection).Settings;
 			}
 				
 			public override string GetPath (string key = null)
 			{
-				string path = "/apps/CHARP";
+				string path = "";
 				if (baseUrl != null) { path += "/" + baseHash; }
 				if (key != null) { path += "/" + key; }
 				return path;
 			}
 
 			public override string Get (string path) {
-				Init ();
-		/*		try {
-					//return gconf.Get (path);
-				} catch (GConf.NoSuchKeyException) {
+				var settings = Init ();
+				if (settings [path] == null)
 					throw new NoSuchKeyException ();
-				}*/
-				return null;
+				return settings [path].Value;
 			}
 
-			public override void Set (string path, string key) {
-				Init ();
-				//gconf.Set (path, key);
+			public override void Set (string path, string value) {
+				var settings = Init ();
+				if (settings [path] == null)
+					settings.Add (path, value);
+				else
+					settings [path].Value = value;
 			}
 
 			public override void Delete (string path) {
-				//Set (path, "");
+				var settings = Init ();
+				settings.Remove (path);
 			}
 
 			public override void SuggestSync () {
-				//gconf.SuggestSync ();
+				config.Save (ConfigurationSaveMode.Modified);
+				ConfigurationManager.RefreshSection (CHARP_SECTION_NAME);
 			}
 		}
 	}
